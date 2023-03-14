20f71bd356f88add3caded61ebdc80a8e939888f
Sauroning <sauroning@yandex.ru>
2023-03-15 00:32:50 +0300

    Commit

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,42 +1,43 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
 public class AdminController {
     private UserService userService;
+    private RoleService roleService;
 
     @Autowired
-    public AdminController(UserService userService) {
+    public AdminController(UserService userService, RoleService roleService) {
         this.userService = userService;
-    }
-
-    @GetMapping(value = "/")
-    public String welcome() {
-        return "redirect:/admin";
+        this.roleService = roleService;
     }
 
     @GetMapping("admin")
-    public String findAll(Model model) {
-        List<User> users = userService.findAll();
-        model.addAttribute("users", users);
+    public String findAll(Model model, Principal principal) {
+        model.addAttribute("users", userService.findAll())
+                .addAttribute("my_user", userService.findByUsername(principal.getName()))
+                .addAttribute("newUser", new User())
+                .addAttribute("rolesAdd", roleService.findAll());
         return "user-list";
     }
 
-    @GetMapping("/user-create")
-    public String createUserForm(User user) {
-        return "user-create";
-    }
-
-    @PostMapping("/user-create")
-    public String createUser(User user) {
+    @PostMapping("/admin")
+    public String createUser(@ModelAttribute("user") User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         userService.saveUser(user);
         return "redirect:/admin";
     }
@@ -47,16 +48,16 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-    @GetMapping("/user-update/{id}")
-    public String updateUserForm(@PathVariable("id") Long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-        return "/user-update";
-    }
-
-    @PatchMapping("/user-update")
-    public String updateUser(User user) {
-        userService.saveUser(user);
+    @PatchMapping("/admin/{id}")
+    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
+        User updateduser = userService.findById(id);
+        updateduser.setName(user.getName());
+        updateduser.setLastName(user.getLastName());
+        updateduser.setAge(user.getAge());
+        updateduser.setUsername(user.getUsername());
+        updateduser.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        updateduser.setRoles(user.getRoles());
+        userService.saveUser(updateduser);
         return "redirect:/admin";
     }
 
